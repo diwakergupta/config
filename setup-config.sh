9394299e391df7ac8ec018141fa5c725b1b6f49a
Diwaker Gupta <diwaker@floatingsun.net>
2011-01-05 12:54:32 -0800

    Helper script to setup config files.

---
+++ setup-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONF_DIR=$(readlink -f $(dirname $0))
+
+ln -sf $CONF_DIR/bashrc $HOME/.bashrc
+ln -sf $CONF_DIR/vimrc $HOME/.vimrc
+ln -sf $CONF_DIR/gvimrc $HOME/.gvimrc
+ln -sf $CONF_DIR/vim $HOME/.vim
+ln -sf $CONF_DIR/gitconfig $HOME/.gitconfig
+ln -sf $CONF_DIR/tmux.conf $HOME/.tmux.conf
