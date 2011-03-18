4a9d77627fa770d868b53c5220dd3ffbbb083a3f
Diwaker Gupta <diwaker@floatingsun.net>
2011-03-18 11:24:49 -0700

    Update eclim

--- setup-config.sh
+++ setup-config.sh
@@ -8,3 +8,4 @@ ln -sf $CONF_DIR/gvimrc $HOME/.gvimrc
 ln -sf $CONF_DIR/vim $HOME/.vim
 ln -sf $CONF_DIR/gitconfig $HOME/.gitconfig
 ln -sf $CONF_DIR/tmux.conf $HOME/.tmux.conf
+ln -sf $CONF_DIR/eclimrc $HOME/.eclimrc
